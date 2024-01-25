d9992a6152a2e5c6bea9db17dc6ef0e3aa700404
Ferîşte-Beep-Boop <angel.munoz692003@gmail.com>
2024-01-25 15:27:16 +0100

    Ajustement Mouvement obj
    j'ai reussi à rien, la ça me rend fou
    
    impossible d'éloigner ou rapprocher l'objet par rapport à la caméra

--- Projet_unity/Assets/Script/TesTSuiviSouris.cs
+++ Projet_unity/Assets/Script/TesTSuiviSouris.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class Souris : MonoBehaviour
 {
     public float lerpSpeed = 30f;
+    public float moveSpeed = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
+        // Obtenez la position du clic de la souris
+        Vector3 posClick = Input.mousePosition;
+
+        Vector3 ChangementLoinZ=new Vector3(0f,0f,-100f);
+         Vector3 ChangementProcheZ=new Vector3(0f,0f,100f);
+
+        // Convertissez les coordonnées de l'écran en coordonnées du monde 3D
+        Vector3 posClickWorld = Camera.main.ScreenToWorldPoint(new Vector3(posClick.x, posClick.y, 10f));
+        Vector3 directionToCamera = (Camera.main.transform.position - transform.position).normalized;
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("Clic gauche pressé");
-
-            // Obtenez la position du clic de la souris
-            Vector3 posClick = Input.mousePosition;
-
-            // Convertissez les coordonnées de l'écran en coordonnées du monde 3D
-            Vector3 posClickWorld = Camera.main.ScreenToWorldPoint(new Vector3(posClick.x, posClick.y, 10f));
-
-            Debug.Log("Coordonnées du clic : X=" + posClickWorld.x + ", Y=" + posClickWorld.y + ", Z=" + posClickWorld.z);
-
             // Interpolez la position actuelle vers la position du clic
             transform.position = Vector3.Lerp(transform.position, posClickWorld, lerpSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.P))
+            {
+                // Rapproche l'objet
+                transform.position += directionToCamera * moveSpeed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.O))
+            {
+                // Eloigne l'objet
+                transform.position -= directionToCamera * moveSpeed * Time.deltaTime;
+            }
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clic gauche pressé");
+            Debug.Log("Coordonnées du clic : X=" + posClickWorld.x + ", Y=" + posClickWorld.y + ", Z=" + posClickWorld.z);
         }
     }
 }
