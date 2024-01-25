51d7fcefe428e23b4f965f7e984600718cf99df1
Ferîşte-Beep-Boop <angel.munoz692003@gmail.com>
2024-01-25 17:07:27 +0100

    dommage
    problème pas reglé

--- Projet_unity/Assets/Script/TesTSuiviSouris.cs
+++ Projet_unity/Assets/Script/TesTSuiviSouris.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Souris : MonoBehaviour
 {
     public float lerpSpeed = 30f;
-    public float moveSpeed = 0.1f;
 
     // Update is called once per frame
     void Update()
@@ -13,27 +12,28 @@ public class Souris : MonoBehaviour
         // Obtenez la position du clic de la souris
         Vector3 posClick = Input.mousePosition;
 
-        Vector3 ChangementLoinZ=new Vector3(0f,0f,-100f);
-         Vector3 ChangementProcheZ=new Vector3(0f,0f,100f);
-
         // Convertissez les coordonnées de l'écran en coordonnées du monde 3D
         Vector3 posClickWorld = Camera.main.ScreenToWorldPoint(new Vector3(posClick.x, posClick.y, 10f));
-        Vector3 directionToCamera = (Camera.main.transform.position - transform.position).normalized;
+        // Bouger l'objet en fonction de la caméra
+        Vector3 directionToCamera = (Camera.main.transform.position - transform.position);
+        Vector3 velocity = Vector3.zero;
+
         if (Input.GetMouseButton(0))
-        {
+        {  
             // Interpolez la position actuelle vers la position du clic
             transform.position = Vector3.Lerp(transform.position, posClickWorld, lerpSpeed * Time.deltaTime);
             if (Input.GetKey(KeyCode.P))
             {
-                // Rapproche l'objet
-                transform.position += directionToCamera * moveSpeed * Time.deltaTime;
+                // Eloigne l'objet
+                velocity += directionToCamera * Time.deltaTime * 10;
             }
             else if (Input.GetKey(KeyCode.O))
             {
-                // Eloigne l'objet
-                transform.position -= directionToCamera * moveSpeed * Time.deltaTime;
+                // Rapproche l'objet
+                transform.Translate(-directionToCamera * Time.deltaTime * 10);
             }
         }
+        transform.Translate(velocity);
         if(Input.GetMouseButtonDown(0))
         {
             Debug.Log("Clic gauche pressé");
