b5b806a5420a06f8c19e7517b5ce51a884c5c3de
Ferîşte-Beep-Boop <angel.munoz692003@gmail.com>
2024-01-25 18:38:01 +0100

    Click que sur l'objet
    Le click fonctionne seleument lorsqu'on touche l'objet

--- Projet_unity/Assets/Script/TesTSuiviSouris.cs
+++ Projet_unity/Assets/Script/TesTSuiviSouris.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+Class servant à bouger les objets avec la souris 
+*/
+
 public class Souris : MonoBehaviour
 {
-    public float lerpSpeed = 30f;
+    public float lerpSpeed = 500f;
 
     // Update is called once per frame
     void Update()
@@ -20,20 +25,34 @@ public class Souris : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {  
-            // Interpolez la position actuelle vers la position du clic
-            transform.position = Vector3.Lerp(transform.position, posClickWorld, lerpSpeed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.P))
+             // Convertit les coordonnées de l'écran en un rayon dans la scène
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Déclare une variable RaycastHit pour stocker les informations sur la collision
+            RaycastHit hit;
+            
+             if (Physics.Raycast(ray, out hit))
             {
-                // Eloigne l'objet
-                velocity += directionToCamera * Time.deltaTime * 10;
-            }
-            else if (Input.GetKey(KeyCode.O))
-            {
-                // Rapproche l'objet
-                transform.Translate(-directionToCamera * Time.deltaTime * 10);
+                // Vérifie si l'objet cliqué est le carré
+                if (hit.collider.gameObject == gameObject)
+                {
+                    // Interpolez la position actuelle vers la position du clic
+                    transform.position = Vector3.Lerp(transform.position, posClickWorld, lerpSpeed * Time.deltaTime);
+                    if (Input.GetKey(KeyCode.P))
+                    {
+                        // Eloigne l'objet
+                        velocity += directionToCamera * Time.deltaTime * 10;
+                    }
+                    else if (Input.GetKey(KeyCode.O))
+                    {
+                        // Rapproche l'objet
+                        transform.Translate(-directionToCamera * Time.deltaTime * 10);
+                    }
+                 }
+                transform.Translate(velocity);
             }
         }
-        transform.Translate(velocity);
+    
         if(Input.GetMouseButtonDown(0))
         {
             Debug.Log("Clic gauche pressé");
